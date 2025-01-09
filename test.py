26b06df05533c4b8e0e570ffa649b2670d7d74e6
WizardHeHeJun <1617186107@qq.com>
2025-01-09 15:22:06 +0800

    版本1.5+tool

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import os
 import joblib
 import torch
 from data import audio_to_spectrogram
@@ -6,7 +7,7 @@ from model import SoundModel
 # 设置设备（GPU或CPU）
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def predict_from_audio(audio_path, model, device, image_size, duration):
+def predict_from_audio(audio_path, model, device, image_size, duration , LABEL_ENCODER ):
 
     # 使用音频处理函数生成频谱图
     spec = audio_to_spectrogram(audio_path, size=image_size, duration=duration)
@@ -21,7 +22,25 @@ def predict_from_audio(audio_path, model, device, image_size, duration):
         prediction = model(spec)
         _, predicted_class = torch.max(prediction, 1)
 
-    return predicted_class.item()
+    # 加载标签编码器
+    label_encoder = joblib.load(LABEL_ENCODER)
+
+    # 解码预测结果
+    predicted_label = label_encoder.inverse_transform(predicted_class.cpu().numpy())
+    return predicted_label[0]
+
+def predict_directory(audio_dir,model, device,image_size, duration , LABEL_ENCODER ):
+
+    predictions = {}
+
+    for root, dirs, files in os.walk(audio_dir):
+        for file in files:
+            if file.endswith('.wav'):  # 只处理 .wav 文件
+                audio_path = os.path.join(root, file)
+                predicted_label = predict_from_audio(audio_path, model , device, image_size , duration , LABEL_ENCODER )
+                predictions[audio_path] = predicted_label
+
+    return predictions
 
 def main():
     # 设置设备
@@ -33,23 +52,30 @@ def main():
     image_size = (128, 128)  # 输入频谱图的尺寸
     duration = 10  # 设定裁剪时长
 
-    # 音频文件路径
-    audio_path = 'E:\Multimodal-analysis-of-infant-crying\data\cry\0G7Hk3D5.wav'
+    #设置模型和标签
+    MODEL='sound_model.pth'
+    LABEL_ENCODER='label_encoder.pkl'
+
+    # # 音频文件路径
+    # audio_path = 'E:\Multimodal-analysis-of-infant-crying\data\hungry\\6A7KZR1p.wav'
+
+    # 音频目录路径
+    audio_dir = 'E:\Multimodal-analysis-of-infant-crying\data\hungry'
 
     # 加载模型
     model = SoundModel(n_classes=n_classes).to(device)
-    model.load_state_dict(torch.load('sound_model.pth'))
+    model.load_state_dict(torch.load(MODEL))
     model.eval()
 
-    # 进行预测
-    predicted_class = predict_from_audio(audio_path, model, device, image_size, duration)
+    # # 对音频文件进行预测
+    # predicted_class = predict_from_audio(audio_path, model, device, image_size, duration)
+    # print(f"Predicted label: {predicted_class}")
 
-    # 加载标签编码器
-    label_encoder = joblib.load('label_encoder.pkl')
-
-    # 获取预测的标签
-    predicted_label = label_encoder.inverse_transform([predicted_class])
-    print(f"Predicted label: {predicted_label[0]}")
+    # 对目录音频进行预测
+    predictions = predict_directory( audio_dir , model, device , image_size , duration , LABEL_ENCODER )
+    print(f"预测结果：")
+    for audio_path, predicted_label in predictions.items():
+        print(f"{audio_path} -> {predicted_label}")
 
 if __name__ == "__main__":
     main()
